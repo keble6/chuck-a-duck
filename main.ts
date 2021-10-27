54121d3ac6ec0b06f591287d4bb753c2a254491f
Rob Wilson <rob@rwilson.org.uk>
2021-10-27 14:35:22 +0100

    added code for > 2 microbits

--- main.ts
+++ main.ts
@@ -1,8 +1,42 @@
 radio.onReceivedString(function (receivedString) {
     basic.showIcon(IconNames.Duck)
+    iAmDuck = 1
 })
 input.onGesture(Gesture.Shake, function () {
-    basic.clearScreen()
     radio.sendString("DUCK")
+    if (iAmDuck) {
+        iAmDuck = 0
+        basic.clearScreen()
+    }
 })
+let iAmDuck = 0
 radio.setGroup(23)
+iAmDuck = 0
+basic.forever(function () {
+    basic.showLeds(`
+        . . . . #
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(100)
+    if (iAmDuck) {
+        basic.showLeds(`
+            . # # . .
+            # # # . .
+            . # # # #
+            . # # # .
+            . . . . .
+            `)
+    } else {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
+    basic.pause(5000)
+})
